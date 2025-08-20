fee3d0e0e3233ea51681a553d80f96b519448f65
Juan P. Farias <juanfariaso@gmail.com>
2025-08-20 09:22:48 -0400

    setup as package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="d-caf",
+    version="0.1.0",
+    description="Dynamic Cluster Assembly Framework (D-CAF)",
+    author="Juan Farias",
+    packages=find_packages(include=["dcaf", "dcaf.*"]),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+    ],
+    python_requires=">=3.11",
+)
